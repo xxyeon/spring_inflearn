e617fe86dfcfc00d218e640bb8b1a78841c33025
xxyeon <jykim9335@gmail.com>
2023-07-17 19:24:16 +0900

    스프링 핵심 원리 이해1 주문과 할인 도메인 실행과 테스트

---
+++ core/src/test/java/com/example/core/order/OrderServiceTest.java
@@ -0,0 +1,23 @@
+package com.example.core.order;
+
+import com.example.core.member.Grade;
+import com.example.core.member.Member;
+import com.example.core.member.MemberService;
+import com.example.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member); //repo에 저장을 해야 나중에 회원정보를 가져올 수 있으므로 join진행
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
