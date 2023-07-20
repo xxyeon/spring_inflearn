f42aaa39b2d4a965dd839dec917f3e0af71c13f7
xxyeon <jykim9335@gmail.com>
2023-07-20 19:17:24 +0900

    스프링 핵심 원리 이해2 AppConfig 리팩터링

--- core/src/main/java/com/example/core/AppConfig.java
+++ core/src/main/java/com/example/core/AppConfig.java
@@ -1,6 +1,8 @@
 package com.example.core;
 
+import com.example.core.discount.DiscountPolicy;
 import com.example.core.discount.FixDiscountPolicy;
+import com.example.core.member.MemberRepository;
 import com.example.core.member.MemberService;
 import com.example.core.member.MemberServiceImpl;
 import com.example.core.member.MemoryMemberRepository;
@@ -10,11 +12,19 @@ import com.example.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
         //MemberServiceImpl은 MemoryMemberRepository를 사용한다고 결정
     }
 
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
     public OrderService orderService() { //orderService에 대한 구체적인 할인 정책을 여기서 선택
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private static DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
