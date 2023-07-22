295c02d777c687c8deaa9029cacc81c6f665711a
xxyeon <jykim9335@gmail.com>
2023-07-22 18:02:31 +0900

    싱글톤 컨테이너 @Configuration과 바이드코드 조작의 마법

--- core/src/test/java/com/example/core/singleton/ConfigurationSingletonTest.java
+++ core/src/test/java/com/example/core/singleton/ConfigurationSingletonTest.java
@@ -32,4 +32,13 @@ public class ConfigurationSingletonTest {
 
 
     }
+    
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+        
+    }
 }
