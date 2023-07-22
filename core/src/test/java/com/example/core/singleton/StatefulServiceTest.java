ae50c6d067c57f2198e8f71571ab6d6eab3ae083
xxyeon <jykim9335@gmail.com>
2023-07-22 17:02:05 +0900

    싱글톤 컨테이너 싱글톤 컨테이너의 주의점

---
+++ core/src/test/java/com/example/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,34 @@
+package com.example.core.singleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+    @Test
+    void  statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        //A가 주문하고 금액을 조회하는 사이에 B가 주문하는 일이 발생
+        //ThreadA: A사용자 10000원 주문
+        int userAPrice = statefulService1.order("userA", 10000);
+        //ThreadB: B사용자 20000원 주문
+        int userBPrice = statefulService2.order("userB", 20000);
+
+        //ThreadA: 사용자A 주문 금액 조회
+//        int price = statefulService1.getPrice();
+        System.out.println("price = " + userAPrice);
+    }
+
+    static class TestConfig {
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+}
