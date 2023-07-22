7e126405b3e9534138e8585f83c771ac56032b41
xxyeon <jykim9335@gmail.com>
2023-07-22 16:09:13 +0900

    싱글톤 컨테이너 싱글톤 패턴

--- core/src/test/java/com/example/core/singleton/SingletonTest.java
+++ core/src/test/java/com/example/core/singleton/SingletonTest.java
@@ -6,6 +6,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class SingletonTest {
 
     //순수한 DI 컨테이너의 문제점과 스프링과 비교
@@ -24,6 +26,20 @@ public class SingletonTest {
         System.out.println("memberService2 = " + memberService2);
 
         //memberService1 ==! memberService2 검증
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+        //smae: == 인스턴스가 같은지 비교
+        //equal: 자바의 equals와 동일한 기능
     }
 }
