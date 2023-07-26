77882e1789f350a19c9d6370488b56af9838f296
xxyeon <jykim9335@gmail.com>
2023-07-26 16:05:38 +0900

    빈 생명주기 콜백 인터페이스InitialisingBean, DisposableBean

--- core/src/test/java/com/example/core/lifecycle/NetworkClient.java
+++ core/src/test/java/com/example/core/lifecycle/NetworkClient.java
@@ -1,6 +1,9 @@
 package com.example.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url; //접속해야하는 서버 url
 
@@ -27,5 +30,18 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    //afterPropertiesSet(): 의존관계 주입이 끝나면 호출
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    //빈이 종료될 때 호출
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
     }
 }
