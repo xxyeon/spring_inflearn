a81e2540ad36a029fd25ee1d61fae47eeba79d17
xxyeon <jykim9335@gmail.com>
2023-07-27 22:00:57 +0900

    빈 스코프 스코프와 프록시

--- core/src/main/java/com/example/core/web/LogDemoController.java
+++ core/src/main/java/com/example/core/web/LogDemoController.java
@@ -13,13 +13,16 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request) {
+    public String logDemo(HttpServletRequest request) throws InterruptedException{
         String requestURL = request.getRequestURI().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
+
+        //MyLogger의 가짜 프록시 클래스를 만들어두고 http request와 상관 없이 가짜 프록시 클래스를 다른 빈에 미리 주입해 둘 수 있다.
+        System.out.println("myLogger = " + myLogger.getClass()); //스프링이 조작해서 만든 애가 스프링 빈으로 등록된다.
+        //myLogger의 기능을 실제 호출하는 시점에서 진짜를 찾아서 동작
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
