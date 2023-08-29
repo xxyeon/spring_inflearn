1ef6435a7c5e344c43e44a3c79fba9f0ccdc4660
xxyeon <jykim9335@gmail.com>
2023-08-29 18:14:21 +0900

    스프링 MVC - 웹 페이지 만들기 PRG Post/Redirect/Get

--- springMVC_1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ springMVC_1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -84,12 +84,17 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV4(Item item) {
         itemRepository.save(item);
         return "basic/item";
     }
 
+    @PostMapping("/add")
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+        return "redirect:/basic/items/" + item.getId();
+    }
     /**
      * 테스트용 데이터 추가
      */
