7e4cd599699ab8ce461e41dff016a2c9b1c11cf2
xxyeon <jykim9335@gmail.com>
2024-06-14 22:24:54 +0900

    섹션 6 주문 도메인 개발

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,6 +11,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //기본 생성자 protected로 지정
 public class OrderItem {
     @Id
     @GeneratedValue
@@ -23,5 +26,26 @@ public class OrderItem {
     @JoinColumn(name = "order_id")
     private Order order;
     private int orderPrice;
-    private int count;
+    private int count; //주문 수량
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        getItem().addStock(count); //주문 취소하면 주문한 재고 수량 번복
+    }
+
+    //==조회 로직==//
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count); //주문하면 재고 수 감소
+        return orderItem;
+    }
 }
