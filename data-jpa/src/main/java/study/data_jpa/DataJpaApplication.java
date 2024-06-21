7c993b034df547714228e705dd9f6437c3dbca2a
xxyeon <jykim9335@gmail.com>
2024-06-21 18:51:16 +0900

    spring data jpa: 공통 인터페이스 기능 1. 순수 jpa기반 리포지토리 만들기 2. 공통 인터페이스 설정 3. 공통 인터페이스 적용 4. 공통 인터페이스 분석

--- data-jpa/src/main/java/study/data_jpa/DataJpaApplication.java
+++ data-jpa/src/main/java/study/data_jpa/DataJpaApplication.java
@@ -2,8 +2,10 @@ package study.data_jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "study.data_jpa.repository")
 public class DataJpaApplication {
 
 	public static void main(String[] args) {
