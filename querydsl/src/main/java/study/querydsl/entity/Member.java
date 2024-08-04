13e636ddaa75ccf7872d54ec399abfa17baff1b3
xxyeon <jykim9335@gmail.com>
2024-08-04 11:26:07 +0900

    Querydsl: 예제 도메인 모델

---
+++ querydsl/src/main/java/study/querydsl/entity/Member.java
@@ -0,0 +1,49 @@
+package study.querydsl.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username) {
+        this(username, 0);
+    }
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    private void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
