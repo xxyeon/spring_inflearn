5e8283c0eab8dc1949225dbfbe6ddeddb3bbd500
xxyeon <jykim9335@gmail.com>
2024-09-27 11:45:46 +0900

    Custom exception: 여러 계층서 Exception 발생시 중간에 받아서 처리하기

--- spring-practice/src/main/java/com/example/spring_practice/travel/TravelService.java
+++ spring-practice/src/main/java/com/example/spring_practice/travel/TravelService.java
@@ -19,19 +19,10 @@ public class TravelService {
             log.info("여행 패키지 예약 완료(리조트 + 항공권");
         }catch (UsernameException | PasswordException | DatabaseException e) {
             /* 1) 내부 고객(개발자)에게 표기할 에러메세지 */
-            log.error("여행 패키지 예약 실패"); //근데 여기서 이렇게 로그를 찍는거 보다
-
-            log.error(e.getMessage());
-            //이렇게 로그를 출력하는게 내부 고객(개발자)에게 좋지않은가? 왜냐면 유저네임이 틀린건지, 비번이 틀린건지 알 수 있으니까
-
-            //외부 고객(사용자) 에게 표시할 에러 (한번 감싸서 던짐)
-            throw new CustomException(2, "RESERVATION_FAILED", "여행 패키지 예약 실패");
+            log.error("여행 패키지 예약 실패 - (3) 중간에 받아서 처리하기");
         } catch (RuntimeException e) {
             //내부 고객(개발자)에게 표기할 에러 메시지
-            log.error("여행 패키지 예약 시 알 수 없는 에러 발생 - (2) 중간에 받아서 다시 넘기기", e);
-
-            //외부 고객(사용자) 에게 표시할 에러
-            throw new CustomException(0, "UNKNOWN_ERROR", "여행 패키지 예약 시 알 수 없는 에러 발생");
+            log.error("여행 패키지 예약 시 알 수 없는 에러 발생 - (3) 중간에 받아서 처리하기", e);
         }
 
 
