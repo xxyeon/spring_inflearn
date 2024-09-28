5565876dc905efee516b25f661b5ccbaf27921ba
xxyeon <jykim9335@gmail.com>
2024-09-28 15:16:06 +0900

    객체 생성하는 방 3가지: 1.빈 생성자 + 수정 통한 필드 주입

--- spring-practice/src/main/java/com/example/spring_practice/SpringPracticeApplication.java
+++ spring-practice/src/main/java/com/example/spring_practice/SpringPracticeApplication.java
@@ -15,20 +15,14 @@ import org.springframework.util.StringUtils;
 public class SpringPracticeApplication {
 	
 	public static void main(String[] args) {
-		Member aaron = new Member("Aaron", "aaron@example.com");
-		Member baron = new Member("Baron", "baron@example.com");
-
-		System.out.println("---");
-		System.out.println(aaron);              // 객체
-		System.out.println(aaron.toString());   // 객체
-		System.out.println(aaron.getName());    // 필드 중 name
-//      System.out.println(aaron.name);         // 필드 중 name (접근제어자 private)
-
-		System.out.println("---");
-		System.out.println(baron);              // 객체
-		System.out.println(baron.toString());   // 객체
-		System.out.println(baron.getName());    // 필드 중 name
-//      System.out.println(baron.name);
+		Member aaron = new Member();
+		aaron.setName("Aaron");
+		aaron.setEmail("aaron@example.com");
+		System.out.println("aaron = " + aaron);
+		Member baron = new Member();
+		baron.setName("Baron");
+		baron.setEmail("baron@example.com");
+		System.out.println("baron = " + baron);
 
 
 	}
--- spring-practice/src/main/java/com/example/spring_practice/member/Member.java
+++ spring-practice/src/main/java/com/example/spring_practice/member/Member.java
@@ -1,22 +1,19 @@
 package com.example.spring_practice.member;
 
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@ToString
 public class Member {
 
-    private Integer id;     // Reference Type 주소를 담는 그릇 : 객체가 담기며, NULL 이 들어갈 수 있다.
+    private Integer id;// Reference Type 주소를 담는 그릇 : 객체가 담기며, NULL 이 들어갈 수 있다.
+    @Setter
     private String name;
     private int age;        // Primitive Type 값을 담는 그릇 : 값 그 자체가 담기며, NULL 이 들어갈 수 없다. (무조건 값이 있어야하기에 기본값 존재)
+    @Setter
     private String email;
 
-    public Member(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public String toString() {
-        return String.format("Member(id=%s, name=%s, age=%s, email=%s)\"", id, name, age, email);
-    }
 }
