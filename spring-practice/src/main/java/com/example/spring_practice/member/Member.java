99c3bde2176aafe9e9273c26e66a7d7867612267
xxyeon <jykim9335@gmail.com>
2024-09-28 16:27:41 +0900

    현업에서 사용하는 Builder 패턴 2. @Builder.Default

--- spring-practice/src/main/java/com/example/spring_practice/member/Member.java
+++ spring-practice/src/main/java/com/example/spring_practice/member/Member.java
@@ -6,12 +6,16 @@ import lombok.Setter;
 import lombok.ToString;
 
 @ToString
+@Builder
 public class Member {
 
     private Integer id;// Reference Type 주소를 담는 그릇 : 객체가 담기며, NULL 이 들어갈 수 있다.
-    private String name;
-    private int age;        // Primitive Type 값을 담는 그릇 : 값 그 자체가 담기며, NULL 이 들어갈 수 없다. (무조건 값이 있어야하기에 기본값 존재)
-    private String email;
+    @Builder.Default
+    private String name = "unNamed";
+    private int age;// Primitive Type 값을 담는 그릇 : 값 그 자체가 담기며, NULL 이 들어갈 수 없다. (무조건 값이 있어야하기에 기본값 존재)
+
+    @Builder.Default
+    private String email = "undefined";
 
     @Builder
     public Member(int age, String email) {
